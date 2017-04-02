7fba48a3cf3ed9cae5fb2e8c3eddff26a5a0babb
lunarmoon26 <jackchang26@gmail.com>
2017-04-02 21:56:08 +0800

    Release 1.1 updates
    1. Created custom editor of PlayerController to add/shuffle/remove
    slides
    2. Updated the logic of Player behavior
    3. Minor bug fixes

--- Assets/Editor/UnityPresentationFramework/PlayerEditor.cs
+++ Assets/Editor/UnityPresentationFramework/PlayerEditor.cs
@@ -1,46 +1,174 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEditor;
+using UnityEditorInternal;
+using UnityPresentationFramework.Control;
+using UnityPresentationFramework.Helpers;
+using System.IO;
 
 namespace UnityPresentationFramework.Inspector{
+
 	[CustomEditor(typeof(PlayerController))]
-	public class PlayerEditor : Editor {
+	public class PlayerEditor : Editor
+    {
 
-		SerializedProperty allSlides;
-		SerializedProperty enabledSlides;
-		SerializedProperty lookAt;
-		SerializedProperty initialSlide;
-
+		SerializedProperty slides;
+        SerializedProperty autoPlay;
 		SerializedProperty cameraRig;
 
 		private string[] m_SlideNames;
 		private string[] m_SlideNamesEnabled;
+        private ReorderableList reorderableSlides;
 
-		void OnEnable(){
-			allSlides = serializedObject.FindProperty("m_AllSlides");
-			enabledSlides = serializedObject.FindProperty("m_EnabledSlides");
-			lookAt = serializedObject.FindProperty ("m_PreviewAt");
-			initialSlide = serializedObject.FindProperty ("m_InitialSlide");
+        private void OnEnable(){
+            slides = serializedObject.FindProperty("m_AllSlides");
 			cameraRig = serializedObject.FindProperty ("m_CameraRig");
-		}
+            autoPlay = serializedObject.FindProperty("m_AutoPlay");
 
-		public override void OnInspectorGUI(){
-			
-			serializedObject.Update ();
+            reorderableSlides = new ReorderableList(serializedObject, slides, true, true, true, true);
+            reorderableSlides.drawHeaderCallback += DrawHeader;
+            reorderableSlides.drawElementCallback += DrawElement;
+            reorderableSlides.onAddDropdownCallback += AddItem;
+            reorderableSlides.onRemoveCallback += RemoveItem;
+            reorderableSlides.onSelectCallback += SelectItem;
+            reorderableSlides.onCanRemoveCallback += CanRemoveItem;
+        }
 
-			m_SlideNames = new string[allSlides.arraySize];
-			for(int i=0; i < allSlides.arraySize; i++){
-				m_SlideNames [i] = allSlides.GetArrayElementAtIndex (i).objectReferenceValue.name;
-			}
-			EditorGUI.BeginChangeCheck();
-			EditorGUILayout.PropertyField(cameraRig);
-			EditorGUILayout.PropertyField (allSlides, true);
-			GUILayout.Label("Look At Slide ");
-			lookAt.intValue = EditorGUILayout.Popup(lookAt.intValue, m_SlideNames);
+        private void OnDisable()
+        {
+            reorderableSlides.drawHeaderCallback -= DrawHeader;
+            reorderableSlides.drawElementCallback -= DrawElement;
+            reorderableSlides.onAddDropdownCallback -= AddItem;
+            reorderableSlides.onRemoveCallback -= RemoveItem;
+            reorderableSlides.onSelectCallback -= SelectItem;
+            reorderableSlides.onCanRemoveCallback -= CanRemoveItem;
+        }
+
+        /// <summary>
+        /// Draws the header of the list
+        /// </summary>
+        /// <param name="rect"></param>
+        private void DrawHeader(Rect rect)
+        {
+            EditorGUI.LabelField(rect, "Slides");
+        }
+
+        /// <summary>
+        /// Draws one element of the list (ListItemExample)
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="index"></param>
+        /// <param name="active"></param>
+        /// <param name="focused"></param>
+        private void DrawElement(Rect rect, int index, bool active, bool focused)
+        {
+            var slide = slides.GetArrayElementAtIndex(index);
+            EditorGUI.PropertyField(rect, 
+                slide, new GUIContent("Slide " + index));
+            //new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight)
+        }
+
+        [MenuItem("Slides/New Slide 1", false, 1)]
+        private void AddItem(Rect buttonRect, ReorderableList list)
+        {
+            var menu = new GenericMenu();
+
+            var themePath = "Assets/Themes";
+            var themes = Directory.GetDirectories(themePath);
+            foreach (string theme in themes)
+            {
+                var guids = AssetDatabase.FindAssets("", new[] { string.Format("{0}/Templates", theme) });
+                foreach (var guid in guids)
+                {
+                    var path = AssetDatabase.GUIDToAssetPath(guid);
+                    menu.AddItem(new GUIContent(string.Format("{0}/{1}", theme.Remove(0, themePath.Length + 1), Path.GetFileNameWithoutExtension(path))),
+                    false, addkHandler,
+                    new SlideCreationParams()
+                    {
+                        Path = path
+                    });
+                }
+            }
 
 
-			if(EditorGUI.EndChangeCheck())
-				serializedObject.ApplyModifiedProperties();
-		}
-	}
+            
+            menu.AddSeparator("");
+            menu.AddItem(new GUIContent("Empty Slide"), false, addkHandler, null);
+            menu.ShowAsContext();
+        }
+
+        private void RemoveItem(ReorderableList list)
+        {
+            if (EditorUtility.DisplayDialog("Warning!",
+                "Are you sure to delete selected slide?", "Yes", "No"))
+            {
+                var controller = slides.GetArrayElementAtIndex(list.index).objectReferenceValue as AbstractSlideController;
+                if (controller)
+                {
+                    if (EditorUtility.DisplayDialog("Warning!",
+                    "Remove GameObject in scene?", "Yes", "No"))
+                    {
+                        DestroyImmediate(controller.gameObject);
+                    }
+                }
+                slides.GetArrayElementAtIndex(list.index).objectReferenceValue = null;
+                ReorderableList.defaultBehaviours.DoRemoveButton(list);
+                Repaint();
+            }
+            
+        }
+
+        private void SelectItem(ReorderableList list)
+        {
+            var slideController = list.serializedProperty.GetArrayElementAtIndex(list.index).objectReferenceValue as AbstractSlideController;
+            if (slideController)
+            {
+                EditorGUIUtility.PingObject(slideController.gameObject);
+                (target as PlayerController).PreviewAtSlide(list.index);
+            }
+        }
+        private bool CanRemoveItem(ReorderableList list)
+        {
+            return list.count > 1;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(cameraRig, new GUIContent("Presentation Camera"));
+            EditorGUILayout.PropertyField(autoPlay);
+            reorderableSlides.DoLayoutList();
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(target, "Changed Slides");
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        private struct SlideCreationParams
+        {
+            public string Path; // Path to template prefab
+        }
+
+        private void addkHandler(object data)
+        {
+            
+            var index = slides.arraySize;
+            slides.arraySize++;
+            reorderableSlides.index = index;
+
+            if (data != null)
+            {
+                var p = (SlideCreationParams)data;
+                var templatePrefab = AssetDatabase.LoadAssetAtPath(p.Path, typeof(GameObject)) as GameObject;
+                slides.GetArrayElementAtIndex(index).objectReferenceValue = SlideHelper.CreateNewSlide(index, templatePrefab, true);
+            }
+            else
+            {
+                slides.GetArrayElementAtIndex(index).objectReferenceValue = null;
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
